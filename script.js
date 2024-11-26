27f3e47ffda6cb9e75907a3efeb69ebb349e9bb5
zareenarif <165668465+zareenarif@users.noreply.github.com>
2024-11-26 20:34:24 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-var toggleButton = document.getElementById('toggle-skills');
-var skills = document.getElementById('skills');
-toggleButton.addEventListener('click', function () {
-    if (skills.style.display === 'none') {
-        skills.style.display = 'block';
+// Get references to the form display area.
+var form = document.getElementById('Resumeform');
+var resumeDisplayElement = document.getElementById('resume-display');
+// handle form submission
+form.addEventListener('submit', function (event) {
+    event.preventDefault(); // prevent page reload
+    //Collect input values
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var phone = document.getElementById('Phone').value;
+    var education = document.getElementById('education').value;
+    var experience = document.getElementById('experience').value;
+    var Skills = document.getElementById('Skills').value;
+    //Genrate resume content dynamically
+    var resumeHTML = "\n    <h2><b>Resume</b></h2>\n    <h3>Personal Information</h3>\n    <p><b>Name:</b>".concat(name, "</p>\n    <p><b>Email:</b>").concat(email, "</p>\n    <p><b>Phone:</b>").concat(phone, "</p>\n\n    <h3>Education</h3>\n    <p>").concat(education, "</p>\n\n     <h3>Experience</h3>\n    <p>").concat(experience, "</p>\n\n     <h3>Skills</h3>\n    <p>").concat(Skills, "</p>\n\n    ");
+    // Display the genrated resume
+    if (resumeDisplayElement) {
+        resumeDisplayElement.innerHTML = resumeHTML;
     }
     else {
-        skills.style.display = 'none';
+        console.error('the resume element is missing');
     }
 });
--- script.ts
+++ script.ts
@@ -1,10 +1,47 @@
-const toggleButton = document.getElementById('toggle-skills') as HTMLButtonElement;
-const skills = document.getElementById('skills') as HTMLElement
+// Get references to the form display area.
 
-toggleButton.addEventListener('click', ()=> {
-    if (skills.style.display === 'none') {
-        skills.style.display = 'block'
-    } else {
-        skills.style.display = 'none'
+const form = document.getElementById('Resumeform') as HTMLFormElement;
+const resumeDisplayElement = document.getElementById('resume-display') as HTMLDivElement;
+
+// handle form submission
+
+form.addEventListener('submit',(event:Event) => {
+    event.preventDefault(); // prevent page reload
+
+    //Collect input values
+
+    const name =(document.getElementById('name')as HTMLInputElement).value
+    const email =(document.getElementById('email')as HTMLInputElement).value
+    const phone =(document.getElementById('Phone')as HTMLInputElement).value
+    const education =(document.getElementById('education')as HTMLInputElement).value
+    const experience =(document.getElementById('experience')as HTMLInputElement).value
+    const Skills =(document.getElementById('Skills')as HTMLInputElement).value
+
+    //Genrate resume content dynamically
+
+    const resumeHTML = `
+    <h2><b>Resume</b></h2>
+    <h3>Personal Information</h3>
+    <p><b>Name:</b>${name}</p>
+    <p><b>Email:</b>${email}</p>
+    <p><b>Phone:</b>${phone}</p>
+
+    <h3>Education</h3>
+    <p>${education}</p>
+
+     <h3>Experience</h3>
+    <p>${experience}</p>
+
+     <h3>Skills</h3>
+    <p>${Skills}</p>
+
+    `;
+
+    // Display the genrated resume
+    if(resumeDisplayElement){
+        resumeDisplayElement.innerHTML = resumeHTML;
+    }
+    else{
+        console.error('the resume element is missing');
     }
 });
